70ece410720ac8ef7569282145f7315f49766a19
69319010033-svg <69319010033@udontech.ac.th>
2026-08-26 16:05:16 +0700

    round2: add student name

--- App.vue/src/App.vue
+++ App.vue/src/App.vue
@@ -1,13 +1,26 @@
-<script setup></script>
-
 <template>
-<div class="container"></div>
+  <div class="container">
+    <h1>{{ studentName }}</h1>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      studentName: "นายดลใจ สุดจริง" // แก้ไขเป็นชื่อ-นามสกุลของคุณเอง
+    };
+  },
+};
+</script>
+
 <style>
 .container {
-width: 100vw;
-height: 100vh;
-background-color: skyblue; /* สีฟ้า */
+  width: 100vw;
+  height: 100vh;
+  background-color: skyblue;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 </style>
-
