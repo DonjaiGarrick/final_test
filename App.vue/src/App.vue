1d254f6368a509771592fd64e4bba8e7e92ff61f
69319010033-svg <69319010033@udontech.ac.th>
2026-08-26 15:38:41 +0700

    round1: change background color

---
+++ App.vue/src/App.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+<div class="container"></div>
+</template>
+<style>
+.container {
+width: 100vw;
+height: 100vh;
+background-color: skyblue; /* สีฟ้า */
+}
+</style>
+
